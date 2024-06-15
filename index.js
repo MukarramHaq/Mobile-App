4774ccfa6ce0a183a175290856ebf38d55b03a6a
MukarramHaq <mukarram796@gmail.com>
2024-06-15 13:49:18 -0400

    Used Grid to display the app in the center

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-cbc0b-default-rtdb.firebaseio.com/"
@@ -23,10 +23,13 @@ addButtonEl.addEventListener("click", function() {
 
 /* Retrieve items from the database */
 onValue(shoppingListInDB, function(snapshot){
-    let groceriesArray = Object.values(snapshot.val())
+    let groceriesArray = Object.entries(snapshot.val())
     clearShoppingListEl()
     for(let i = 0; i < groceriesArray.length; i++){
-        appendItemShoppingListEl(groceriesArray[i])
+        let currentItem = groceriesArray[i]
+        let currentItemID = groceriesArray[0]
+        let currentItemValue = groceriesArray[1]
+        appendItemShoppingListEl(currentItem)
     }
 })
 
@@ -34,8 +37,20 @@ function clearShoppingListEl(){
     shoppingListEl.innerHTML = ""
 }
 
-function appendItemShoppingListEl(inputValue){
-    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+function appendItemShoppingListEl(item){
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+
+    newEl.textContent = itemValue
+
+    newEl.addEventListener("click", function(){
+        let locationOfItemToBeDeleted = ref(database, `shoppingList/${itemID}`)
+        remove(locationOfItemToBeDeleted)
+    })
+
+    shoppingListEl.append(newEl)
 }
 
 function clearInputField(){
